a6e029997f5634e1367dd56dc0b682e89391302a
przemek milke <theblimek@gmail.com>
2024-03-05 09:55:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Podaj długość podstawy: ");
+            double x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj długość boku:");
+            double y = Int32.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(Math.Pow( x, 2) + Math.Pow(y, 2));
+
+            Console.WriteLine($"Przekątna tego prostokąta wynosi : {result};");
+            Console.ReadLine();
+            
+
+
+
+
+            
+        }
+    }
+}
